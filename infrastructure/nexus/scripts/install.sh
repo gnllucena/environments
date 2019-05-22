29dddefb73e635ba36ba7280c78e09f725303e1d
Gabriel Lucena <gnllucena@gmail.com>
2019-05-22 02:59:59 -0300

    Experimentos com Nexus seguem.

--- infrastructure/nexus/scripts/install.sh
+++ infrastructure/nexus/scripts/install.sh
@@ -7,10 +7,13 @@ if [ "$(helm status nexus)" ]; then
   exit 0
 fi
 
+
+# https://stackoverflow.com/questions/42766349/run-nexus-3-with-docker-in-a-kubernetes-cluster
+
 echo "===== NEXUS : INSTALLING" # https://github.com/helm/charts/tree/master/stable/sonatype-nexus
 helm install stable/sonatype-nexus \
      --name nexus \
      --namespace nexus \
      --version 1.18.1 \
      --values ./../configurations/chart/values.yaml \
-     --set nexusProxy.env.nexusDockerHost=localhost,nexusProxy.env.nexusHttpHost=localhost,nexus.service.clusterIP=10.100.109.66
+     --set nexusProxy.env.nexusDockerHost=nexus.localhost,nexusProxy.env.nexusHttpHost=nexus.localhost,ingress.enabled=true
